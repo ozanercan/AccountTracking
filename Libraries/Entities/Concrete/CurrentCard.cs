73c34b3af3b857dc2b0c6a29168d48630baeb46a
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2021-05-02 01:36:51 +0300

    Corporate & individual current card models added.

---
+++ Libraries/Entities/Concrete/CurrentCard.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{   // bireysel ve kurumsal carilere inherit edilen class
+    // telefon numarası, email, adres sınırsız tutulabileceğinden türleri liste
+    public class CurrentCard : IEntity
+    {
+        public List<CurrentCardPhoneNumber> PhoneNumbers { get; set; }
+        public List<CurrentCardEmail> Emails { get; set; }
+        public List<CurrentCardAddress> Addresses { get; set; }
+    }
+}
---
+++ Libraries/Entities/Concrete/CurrentCardAddress.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{   // CurrentCard'da liste olarak tutulan Addresses prop'unun nesnesi
+    public class CurrentCardAddress : IEntity
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+    }
+}
